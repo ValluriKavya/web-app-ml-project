2f6acf1a421e79b4b790298fa5a8e94643371ec8
ValluriKavya <155796024+ValluriKavya@users.noreply.github.com>
2024-01-06 13:00:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ ph=st.number_input('Enter pH:')
 r=st.number_input('Enter Rainfall:')
 if st.button('Recommend Crop'):
     data=[[n,p,t,k,h,ph,r]]
-    result=classifier.predict(data)(0)
+    result=classifier.predict(data)[0]
     st.success(result)
